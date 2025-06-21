439bc0696b5c7679501400236e3382f017aacc8f
SauraGDeshmukh <128806572+saurabhgdeshmukh@users.noreply.github.com>
2025-06-21 09:33:05 +0530

    Update QuestionList.jsx

--- client/src/pages/teachers/QuestionList.jsx
+++ client/src/pages/teachers/QuestionList.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import io from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://live-polling-system-1-zunt.onrender.com");
 function QuestionList() {
   const navigate = useNavigate();
   const [question, setQuestion] = useState("");
